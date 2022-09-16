6f22f1bafd8a60e826b9d63f02041d9961b5b081
Sebastian <sebastian.sc1990@gmail.com>
2022-09-16 11:27:06 -0300

    asd

--- src/python/readme.md
+++ src/python/readme.md
@@ -58,3 +58,55 @@ def VerificarGanador(jugador,computador):
       
 print(ObtenerOpciones())
 ```
+
+
+-  **Comentario**
+```python
+# esto es un comentario en python
+```
+
+-  **Verificar tipo de variable**
+```python
+print(type("asd")==str) # true
+print(isinstance("asd",str))# true
+print(isinstance(3,float))# false
+print(isinstance(float(3),float))#true, se usa el casteo implicito
+```
+
+-  **String Method**
+```python
+print("sebastian".upper()) # SEBASTIAN
+print("sebastian".title()) # Sebastian
+print("Seba" in "Sebastian") # in es como el contains
+```
+
+-  **any**
+```python
+# Devuelve true si algun valor del enumerable es verdadero
+libro1 = True
+libro2 = False
+
+existeLibro = any([libro1,libro2])
+print(existeLibro) # true
+```
+
+-  **Enum**
+```python
+class Estado(Enum):
+  INACTIVO =0 #son constantes que no cambian, ya que en python no se pueden declarar constantes
+  ACTIVO= 1
+
+print(Estado.ACTIVO.value) #1
+print(Estado.ACTIVO) #Estado.ACTIVO
+print(Estado(1)) #Estado.ACTIVO
+```
+
+-  **Listas**
+```python
+lista = ["agustin","toby",1,False]
+print(lista[0]) #agustin
+print("agustin" in lista) #true
+lista.append("Seba") #agrega un nuevo elemento a la lista
+print(lista[-1]) # devuelve el ultimo en la lista
+```
+
--- src/python/test.py
+++ src/python/test.py
@@ -1,26 +1,64 @@
-import random
-
-def ObtenerOpciones():
-  opcionJugador = input("Eliga una opcion (Piedra,Papel,Tijera)")
-  opciones = ["piedra","papel","tijera"]
-  opcionComputador = random.choice(opciones)
-  resultado = VerificarGanador(opcionJugador,opcionComputador)
-  return resultado
+# import random
 
 
-def VerificarGanador(jugador,computador):
-  print(f"Jugador: {jugador} --- Computador: {computador}")
-  if (jugador==computador):
-    return "Es un Empate"
-  elif(jugador == "piedra" and computador == "tijera"):
-    return "Jugador Gana"
-  elif(jugador == "papel" and computador == "piedra"):
-    return "Jugador Gana"
-  elif(jugador == "tijera" and computador == "papel"):
-    return "Jugador Gana"
-  elif(jugador !="papel" and jugador !="piedra" and jugador != "tijera"):
-    return "Debe ingresar una opcion valida"
-  else:
-    return "Computador gana"
+# def ObtenerOpciones():
+#   opcionJugador = input("Eliga una opcion (Piedra,Papel,Tijera)")
+#   opciones = ["piedra", "papel", "tijera"]
+#   opcionComputador = random.choice(opciones)
+#   resultado = VerificarGanador(opcionJugador, opcionComputador)
+#   return resultado
+
+
+# def VerificarGanador(jugador,computador):
+#   print(f"Jugador: {jugador} --- Computador: {computador}")
+#   if (jugador==computador):
+#     return "Es un Empate"
+#   elif(jugador == "piedra" and computador == "tijera"):
+#     return "Jugador Gana"
+#   elif(jugador == "papel" and computador == "piedra"):
+#     return "Jugador Gana"
+#   elif(jugador == "tijera" and computador == "papel"):
+#     return "Jugador Gana"
+#   elif(jugador !="papel" and jugador !="piedra" and jugador != "tijera"):
+#     return "Debe ingresar una opcion valida"
+#   else:
+#     return "Computador gana"
       
-print(ObtenerOpciones())
+# print(ObtenerOpciones())
+
+
+# print(type("asd")==str)
+# print(isinstance("asd",str))
+# print(isinstance(3,float))
+# print(isinstance(float(3),float))
+# print("Seba" in "Sebastian") # Sebastian
+# print("Sebnastian"[1]) #devuelve la posision 1 es decir la e
+# print("Sebnastian"[0:4]) #Sebn
+
+# libro1 = True
+# libro2 = False
+
+# existeLibro = any([libro1,libro2])
+# print(existeLibro) # true
+
+
+# from enum import Enum
+
+
+# class Estado(Enum):
+#   INACTIVO =0 #son constantes que no cambian
+#   ACTIVO= 1
+
+
+# print(Estado.ACTIVO.value) #1
+# print(Estado.ACTIVO) #Estado.ACTIVO
+# print(Estado(1)) #Estado.ACTIVO
+
+
+lista = ["agustin","toby",1,False]
+
+print(lista[0]) #agustin
+print("agustin" in lista) #true
+lista.append("Seba") #agrega un nuevo elemento a la lista
+print(lista[-1]) # devuelve el ultimo en la lista
+
